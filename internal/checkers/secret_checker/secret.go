245ef53a71377356ecb89b92c87488bf9e708e3e
kmarkela <kmarkela@cisco.com>
2024-03-10 16:17:38 +0100

    secterts

---
+++ internal/checkers/secret_checker/secret.go
@@ -0,0 +1,96 @@
+package secretchecker
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+
+	"github.com/kmarkela/Wiggumizeng/internal/historyparser"
+	"github.com/kmarkela/Wiggumizeng/internal/scanner/splugin"
+	"gopkg.in/yaml.v2"
+)
+
+var configFile string = "internal/checkers/secret_checker/config.yaml"
+
+type RulesList struct {
+	Rules []Rule `yaml:"rules"`
+}
+
+type Rule struct {
+	Description string `yaml:"description"`
+	Regex       string `yaml:"regex"`
+}
+
+type secretChecker struct {
+	name, descr string
+}
+
+// declare checker
+func New() secretChecker {
+	return secretChecker{
+		name:  "subdomain_checker",
+		descr: "This module is searching for 404 messages form hosting platformas",
+	}
+}
+
+func (sc secretChecker) GetName() string {
+	return sc.name
+}
+
+func (sc secretChecker) GetDescr() string {
+	return sc.descr
+}
+
+func getRulesList() ([]Rule, error) {
+	file, err := os.ReadFile(configFile)
+	if err != nil {
+		return []Rule{}, err
+	}
+
+	var rList RulesList
+	if err := yaml.Unmarshal(file, &rList); err != nil {
+		return []Rule{}, err
+	}
+	return rList.Rules, nil
+}
+
+func (sc secretChecker) Check(hi *historyparser.HistoryItem) (splugin.Finding, bool) {
+	var f splugin.Finding
+
+	rl, err := getRulesList()
+	if err != nil {
+		log.Printf("Cannot get list of not found messages. Err: %s\n", err.Error())
+		return f, false
+	}
+
+	for _, rule := range rl {
+
+		regex, err := regexp.Compile(rule.Regex)
+		if err != nil {
+			log.Printf("Error compiling regex pattern: %s\n", err)
+			continue
+		}
+
+		match := regex.FindString(hi.Req.Body + hi.Req.Headers + hi.Res.Body + hi.Res.Headers)
+
+		if match == "" {
+			continue
+		}
+
+		f = splugin.Finding{
+			Host:        hi.Host,
+			Description: "Secrets Found",
+			Evidens:     fmt.Sprintf("Path: %s", hi.Path),
+		}
+
+		f.Details = f.Details + fmt.Sprintf("Description: %s\n Match: %s\n", rule.Description, match)
+
+	}
+
+	if f.Host != "" {
+		return f, true
+	}
+
+	return f, false
+}
