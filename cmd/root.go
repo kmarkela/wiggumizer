8e272999f430e175fe30a45279c888395c3f8e4d
kmarkela <kmarkela@cisco.com>
2024-04-04 18:25:07 +0200

    parameters

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "v0.3.0-alpha"
+const version = "v0.3.1"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
